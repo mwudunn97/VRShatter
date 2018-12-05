b94c3ce2431e19f9f1ca77be6baae0f3b1fef77c
md <mdune50@gmail.com>
2018-12-05 13:13:04 -0800

    Added pause for oculus

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -11,7 +11,7 @@ public class Pause : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || OVRInput.Get(OVRInput.Button.Two))
         {
             if (!pausePanel.activeInHierarchy)
             {
