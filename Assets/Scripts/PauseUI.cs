6e417a0146fe3a248392a100f00360a54cf93ebc
md <mdune50@gmail.com>
2018-12-05 13:40:53 -0800

    Add pause ui

--- Assets/Scripts/PauseUI.cs
+++ Assets/Scripts/PauseUI.cs
@@ -7,13 +7,14 @@ public class PauseUI : MonoBehaviour {
     public Transform location;
 	// Use this for initialization
 	void Start () {
-        gameObject.transform.position = location.position + location.forward * 2.0f;
-        gameObject.transform.rotation = location.rotation;
+        GetComponent<RectTransform>().position = location.position + location.forward * 2.0f;
+        GetComponent<RectTransform>().rotation = location.rotation;
+        this.gameObject.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.position = location.position + location.forward * 2.0f;
-        gameObject.transform.rotation = location.rotation;
+        GetComponent<RectTransform>().position = location.position + location.forward * 2.0f;
+        GetComponent<RectTransform>().rotation = location.rotation;
     }
 }
