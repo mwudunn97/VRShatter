e3da0fbdc476c71ac3de291f0657c57290e41eb5
md <mdune50@gmail.com>
2018-12-05 13:33:26 -0800

    Pause UI script

---
+++ Assets/Scripts/PauseUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseUI : MonoBehaviour {
+
+    public Transform location;
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.position = location.position + location.forward * 2.0f;
+        gameObject.transform.rotation = location.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = location.position + location.forward * 2.0f;
+        gameObject.transform.rotation = location.rotation;
+    }
+}
