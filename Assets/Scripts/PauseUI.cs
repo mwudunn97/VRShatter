f4ff6168526424691d2990685601f67daa70835c
mwudunn <paxtanlaker@berkeley.edu>
2018-12-05 16:08:15 -0800

    Last minute fix

--- Assets/Scripts/PauseUI.cs
+++ Assets/Scripts/PauseUI.cs
@@ -7,14 +7,14 @@ public class PauseUI : MonoBehaviour {
     public Transform location;
 	// Use this for initialization
 	void Start () {
-        GetComponent<RectTransform>().position = location.position + location.forward * 2.0f;
+        GetComponent<RectTransform>().position = location.position + location.forward * 4.0f;
         GetComponent<RectTransform>().rotation = location.rotation;
         this.gameObject.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<RectTransform>().position = location.position + location.forward * 2.0f;
+        GetComponent<RectTransform>().position = location.position + location.forward * 4.0f;
         GetComponent<RectTransform>().rotation = location.rotation;
     }
 }
